9cac838ed096de0d1fa335e08b3b6ac5b1589623
Karl Mathias Moberg <karlmathias@gmail.com>
2021-09-28 16:16:41 +0200

    Dig slightly into the JSON response to give a clean response of the station names

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Author: Karl Mathias Moberg
 # License: MIT
 
-import os, requests
+import os, requests, json
 from flask import Flask
 
 api = Flask(__name__)
@@ -15,13 +15,28 @@ def get_stations():
     api_url = os.getenv('API_URL')
 
     try:
-        stations = requests.get(api_url)
-        stations_json_payload = stations.json()
+        stations_raw = requests.get(api_url)
+
+        stations_json_payload = stations_raw.json()
+
+        # The rest here depends on how the tasks should be completed.
+        # It's kinda ambigious in the task so either just return the previous variable for the pure, raw code
+        # Or if you just want the names of the stations, use the following
+
+        stations_list = stations_json_payload['data']['stations']
+
+        all_stations = []
+
+        for i in range(len(stations_list)):
+            all_stations.append(stations_list[i]['name'])
 
     except:
         print('ERROR: {}'.format())
 
-    return stations_json_payload
+    return json.dumps(all_stations, ensure_ascii=False).encode('utf8')  # If you want the entire JSON dump and, comment line.
+
+    # Uncomment this if you want to return absolutely everything, raw, then commend the previous line.
+    # return stations_json_payload
 
 
 if __name__ == '__main__':
